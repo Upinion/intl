0c4526c1d039d8bf9b4f85f00de4ac2cff62d983
Rik Smit <rik@upinion.com>
2022-06-15 16:40:49 +0200

    First try for the script to create a new index file without the dynamic imports

---
+++ scripts/replaceDynamicImports.js
@@ -0,0 +1,25 @@
+const _ = require('underscore');
+const locales = require('../locales.json');
+
+const fs = require('fs');
+const path = require('path');
+
+const indexFilename = path.join(__dirname, '../index.js');
+const mobileIndexFilename = path.join(__dirname, '../mobile_index.js');
+const file = fs.readFileSync(indexFilename, 'utf8');
+
+let localeDataImport = 'const LocaleData = {};\n';
+
+const languageNames = Object.keys(locales).map(locale => locale.split('_')[0]);
+
+_.uniq(languageNames).forEach((languageName) => {
+    localeDataImport += (
+        `LocaleData.${languageName} = require('react-native-experimental-intl/locale-data/${languageName}');\n`
+    );
+});
+
+const newFile = file.replace(/const LocaleData = {};[\s\S]+\n\nmodule/gi, `${localeDataImport}\nmodule`);
+
+fs.writeFileSync(mobileIndexFilename, newFile);
+
+console.log('Dynamic locale data file successfully generated');
